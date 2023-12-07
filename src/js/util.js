55a25fd26b7a8b70c3f43a18db321d4ef51ddcbe
wenruo0522 <376567610@qq.com>
2023-12-08 01:53:13 +0800

    feat: sortedLinkedList && example

--- src/js/util.js
+++ src/js/util.js
@@ -1,8 +1,19 @@
 
-function defaultEquals(a, b) {
-    return a === b
+export const Compare = {
+    LESS_THAN: -1,
+    BIGGER_THAN: 1,
+    EQUALS: 0
 }
 
-export {
-    defaultEquals
+
+export function defaultCompare(a, b) {
+    if (a === b) {
+       return Compare.EQUALS
+    }
+    return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
+}
+
+
+export function defaultEquals(a, b) {
+    return a === b
 }
